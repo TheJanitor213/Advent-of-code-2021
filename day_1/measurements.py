3fddf943bf6664821bf55fb6854716a65bfb7afb
thejanitor213 <brucebeck995@gmail.com>
2021-12-19 14:49:53 +0200

    Part 2 completed

--- day_1/measurements.py
+++ day_1/measurements.py
@@ -6,10 +6,19 @@ def calc(argv):
         measurements = f.readlines()
         measurements = [measure.rstrip() for measure in measurements] #data file seemed to have \n at the end of each line
     counter = 0
-    for count, measure in enumerate(measurements[1:]):
-        if int(measure) > int(measurements[count-1]):
+    sum_counter = 0
+    previous_sum = 0
+    for index in range(len(measurements) - 1):
+        if int(measurements[index + 1]) > int(measurements[index]):
             counter += 1
-    return counter
+        previous_sum = int(measurements[index - 1]) + int(measurements[index]) + int(measurements[index + 1])
+        try:
+            current_sum = int(measurements[index]) + int(measurements[index + 1]) + int(measurements[index + 2])
+        except:
+            continue
+        if current_sum > previous_sum:
+            sum_counter += 1
+    return counter, sum_counter
 
 if __name__ == '__main__':
     print(calc(sys.argv[1]))
