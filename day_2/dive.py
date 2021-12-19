507f7873f9b6faadf325f203fc4d55c226a9c12d
thejanitor213 <brucebeck995@gmail.com>
2021-12-19 20:26:16 +0200

    Day 2 answer complete

---
+++ day_2/dive.py
@@ -0,0 +1,21 @@
+import sys
+
+def coordinates(argv):
+    depth, horizontal = 0, 0
+    with open(argv,'r') as f:
+        movements = f.readlines()
+        movements = [movement.rstrip() for movement in movements] #data file seemed to have \n at the end of each line
+
+    for movement in movements:
+        direction, distance = movement.split((' '))
+
+        if direction == 'forward':
+            horizontal += int(distance)
+        elif direction == 'up':
+            depth -= int(distance)
+        else:
+            depth += int(distance)
+    return depth * horizontal
+
+if __name__ == '__main__':
+    print(coordinates(sys.argv[1]))
